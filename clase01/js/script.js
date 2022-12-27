fdf803e317361b692f233ac299c01d9380bc4f73
Julieta Pereira <julieta.peduarte@gmail.com>
2022-12-27 19:04:06 -0300

    agregando archivos a mi proyecto

---
+++ clase01/js/script.js
@@ -0,0 +1,11 @@
+let nombre = "Julieta" ;
+let apellido = "Pereira" ;
+let edad = "30" ;
+let edadmas = "10" ;
+console.log("HOLA CHICOS DE CODER") ;
+console.log("MI NOMBRE ES: " , nombre) ;
+console.log("y mi edad es: "+(edad+edadmas));
+
+let entrada = prompt("Ingresar una letra");
+let salida = entrada + " " + "ingresada";
+alert(salida);
